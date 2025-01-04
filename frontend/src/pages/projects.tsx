3a9fe949cfbcc7d0b4f22a9964e5773206711dd6
Biraj Buddhacharya <otaku000god@gmail.com>
2025-01-04 21:56:15 +0545

    reusable card component for project section

--- frontend/src/pages/projects.tsx
+++ frontend/src/pages/projects.tsx
@@ -2,6 +2,32 @@ import FadeInSection from "../components/fadeIn"; // Make sure the path is corre
 import "../assets/css/spinningProjects.css";
 import rightArrow from "../assets/icons/rightArrow.png";
 import { useState } from "react";
+import dockerLogo from "../assets/icons/docker.svg";
+
+function SpinningCard({
+  imgUrl,
+  heading,
+  discription,
+  buttonLink,
+  CardId,
+}: {
+  imgUrl: string;
+  heading: string;
+  discription: string;
+  buttonLink: string;
+  CardId: string;
+}) {
+  return (
+    <>
+      <div className="flex flex-col justify-center items-center spinningCard text-black" id={CardId}>
+        <img src={imgUrl} className="h-20 w-20 bg-cover bg-no-repeat"/>
+        <h2 className="text-xl">{heading}</h2>
+        <p className="text-sm">{discription}</p>
+        <a target="_blank" className="text-base py-2 px-5 bg-primary rounded-md text-white hover:text-white" href={buttonLink}>Learn More</a>
+      </div>
+    </>
+  );
+}
 
 function Projects() {
   const [rotation, setRotation] = useState(0);
@@ -57,7 +83,13 @@ function Projects() {
               style={{ transform: `rotate(${rotation}deg)` }}
             >
               <div>
-                <div id="spinningCard1" className="spinningCard"></div>
+                <SpinningCard
+                  CardId="spinningCard1"
+                  heading="ABC BOOKS"
+                  discription="ecommerce website"
+                  buttonLink="https://www.github.com/BirajBuddhacharya/ABC-Books"
+                  imgUrl={dockerLogo}
+                />
               </div>
               <div>
                 <div id="spinningCard2" className="spinningCard"></div>
