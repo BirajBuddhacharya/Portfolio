51ebee566331ec6a5460344b67883a0034b4a7de
Biraj Buddhacharya <otaku000god@gmail.com>
2025-01-06 22:12:45 +0545

    min and max rotation: project section

--- frontend/src/pages/projects.tsx
+++ frontend/src/pages/projects.tsx
@@ -19,11 +19,20 @@ function SpinningCard({
 }) {
   return (
     <>
-      <div className="flex flex-col justify-center items-center spinningCard text-black" id={CardId}>
-        <img src={imgUrl} className="h-20 w-20 bg-cover bg-no-repeat"/>
+      <div
+        className="flex flex-col justify-center items-center spinningCard text-black"
+        id={CardId}
+      >
+        <img src={imgUrl} className="h-20 w-20 bg-cover bg-no-repeat" />
         <h2 className="text-xl">{heading}</h2>
         <p className="text-sm">{discription}</p>
-        <a target="_blank" className="text-base py-2 px-5 bg-primary rounded-md text-white hover:text-white" href={buttonLink}>Learn More</a>
+        <a
+          target="_blank"
+          className="text-base py-2 px-5 bg-primary rounded-md text-white hover:text-white"
+          href={buttonLink}
+        >
+          Learn More
+        </a>
       </div>
     </>
   );
@@ -31,6 +40,7 @@ function SpinningCard({
 
 function Projects() {
   const [rotation, setRotation] = useState(0);
+  const [minRotation, maxRotation] = [-45, 45];
 
   const handleRightClick = () => {
     setRotation(rotation - 45);
@@ -47,32 +57,40 @@ function Projects() {
           <span className="text-primary">Projects</span>
         </h1>
         <div className="flex justify-center items-center gap-10">
-          <button
-            className="p-3 rotate-180 bg-transparent border-primary rounded-full"
-            onClick={handleLeftClick}
-          >
-            <div
-              className="h-6 w-6"
-              style={{
-                backgroundImage: `url(${rightArrow})`,
-                backgroundSize: "cover",
-                backgroundRepeat: "no-repeat",
-              }}
-            ></div>
-          </button>
-          <button
-            className="p-3 rotate-180 bg-transparent border-primary rounded-full"
-            onClick={handleRightClick}
-          >
-            <div
-              className="h-6 w-6 rotate-180"
-              style={{
-                backgroundImage: `url(${rightArrow})`,
-                backgroundSize: "cover",
-                backgroundRepeat: "no-repeat",
-              }}
-            ></div>
-          </button>
+          <div className="h-12 w-12">
+            {rotation < maxRotation && (
+              <button
+                className="h-full w-full p-3 rotate-180 bg-transparent border-primary rounded-full"
+                onClick={handleLeftClick}
+              >
+                <div
+                  className="h-full w-full"
+                  style={{
+                    backgroundImage: `url(${rightArrow})`,
+                    backgroundSize: "cover",
+                    backgroundRepeat: "no-repeat",
+                  }}
+                />
+              </button>
+            )}
+          </div>
+          <div className="h-12 w-12">
+            {rotation > minRotation && (
+              <button
+                className="h-full w-full p-3 rotate-180 bg-transparent border-primary rounded-full"
+                onClick={handleRightClick}
+              >
+                <div
+                  className="h-full w-full rotate-180"
+                  style={{
+                    backgroundImage: `url(${rightArrow})`,
+                    backgroundSize: "cover",
+                    backgroundRepeat: "no-repeat",
+                  }}
+                />
+              </button>
+            )}
+          </div>
         </div>
         <div id="roundedDivWrapper">
           <div className="relative">
@@ -92,28 +110,40 @@ function Projects() {
                 />
               </div>
               <div>
-                <div id="spinningCard2" className="spinningCard"></div>
+                <SpinningCard
+                  CardId="spinningCard2"
+                  heading="ABC BOOKS"
+                  discription="ecommerce website"
+                  buttonLink="https://www.github.com/BirajBuddhacharya/ABC-Books"
+                  imgUrl={dockerLogo}
+                />
               </div>
               <div>
-                <div id="spinningCard3" className="spinningCard"></div>
+                <SpinningCard
+                  CardId="spinningCard3"
+                  heading="ABC BOOKS"
+                  discription="ecommerce website"
+                  buttonLink="https://www.github.com/BirajBuddhacharya/ABC-Books"
+                  imgUrl={dockerLogo}
+                />
               </div>
               <div>
-                <div id="spinningCard4" className="spinningCard"></div>
+                {/* <div id="spinningCard4" className="spinningCard"></div> */}
               </div>
               <div>
                 {/* <div id="spinningCard5" className="spinningCard"></div> */}
               </div>
               <div>
-                <div id="spinningCard6" className="spinningCard"></div>
+                {/* <div id="spinningCard6" className="spinningCard"></div> */}
               </div>
               <div>
-                <div id="spinningCard7" className="spinningCard"></div>
+                {/* <div id="spinningCard7" className="spinningCard"></div> */}
               </div>
               <div>
-                <div id="spinningCard8" className="spinningCard"></div>
+                {/* <div id="spinningCard8" className="spinningCard"></div> */}
               </div>
               <div>
-                <div id="spinningCard9" className="spinningCard"></div>
+                {/* <div id="spinningCard9" className="spinningCard"></div> */}
               </div>
             </div>
           </div>
