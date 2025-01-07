79430de8ea887a8205bf6532594803af127a92c6
Biraj Buddhacharya <otaku000god@gmail.com>
2025-01-07 19:55:32 +0545

    vite.config Update: LAN Hosting

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // or use '0.0.0.0'
+    port: 3000, // Optional: Set a specific port
+  },
   // Base URL configuration for deployment
   base: '/',
   build: {
