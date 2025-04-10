94d3613252793e932619642479c9f64475889642
Biraj Buddhacharya <otaku000god@gmail.com>
2025-04-10 11:11:04 +0545

    Bug fix: Email no new line fix

--- src/pages/api/contact.ts
+++ src/pages/api/contact.ts
@@ -2,22 +2,24 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 import { Resend } from 'resend';
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+    // validation
     if (req.method !== "POST") {
         return res.status(405).json({ error: "Method not allowed" });
     }
 
     const { RESEND_API_KEY, CONTACT_EMAIL } = process.env;
-
     if (!RESEND_API_KEY || !CONTACT_EMAIL) {
         return res.status(500).json({ error: "Server configuration error" });
     }
 
-    const { message, email } = req.body; // Destructure directly from the body
-
+    const { message, email } = req.body; 
     if (!message || !email) {
         return res.status(400).json({ error: "Message and email are required" });
     }
+    // configuring message
+    const formattedMessage = message.replace(/\n/g, '<br>');
 
+    // processing message
     try {
         const resend = new Resend(RESEND_API_KEY);
 
@@ -25,11 +27,13 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
             from: 'WebsiteContact@resend.dev',
             to: CONTACT_EMAIL,
             subject: `Message from ${email}`,
-            html: message,
+            html: formattedMessage,
         }); 
 
         return res.status(200).json({ message: "Thanks for contacting me ;)" });
-    } catch (error) {
+    } 
+    // error handle 
+    catch (error) {
         console.error("Error sending email:", error);
         return res.status(500).json({ error: "Failed to send email" });
     }
