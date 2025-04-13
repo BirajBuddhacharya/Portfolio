e46925cc87b39f6bdbc7e3efc164efb928843d1f
BirajBuddhacharya <otaku000god@gmail.com>
2025-04-13 12:50:10 +0545

    animation: skill card animation

--- src/components/SpinningCard.tsx
+++ src/components/SpinningCard.tsx
@@ -1,3 +1,6 @@
+'use client'
+import { motion, useAnimation } from 'framer-motion';
+
 export default function SpinningCard({
   imgUrl,
   heading,
@@ -11,28 +14,37 @@ export default function SpinningCard({
   buttonLink: string;
   CardId: string;
 }) {
+  const controls = useAnimation()
   return (
     <>
-      <div
-        className="flex flex-col items-start text-black bg-[#1A1A23] w-full h-96 absolute top-0 left-0 rounded-lg p-2"
+      <motion.div
+        onHoverStart={() => controls.start({ scale: 1.2 })}
+        onHoverEnd={() => controls.start({ scale: 1 })}
+        className="flex flex-col items-start text-black bg-[#1A1A23] w-full h-96 absolute top-0 left-0 rounded-lg p-2 "
         id={CardId}
       >
-        <div
-          style={{ backgroundImage: `url(${imgUrl})` }}
-          className="h-3/4 w-full bg-cover bg-no-repeat rounded-lg"
-        />
+        <div className="relative w-full h-full overflow-hidden rounded-lg">
+          <motion.div
+            style={{ backgroundImage: `url(${imgUrl})` }}
+            className="absolute top-0 left-0 w-full h-full bg-cover bg-no-repeat"
+            animate = {controls}
+            transition={{ duration: 0.2 }}
+          />
+        </div>
         <h2 className="text-xl w-full text-center mb-2 text-white">{heading}</h2>
         <p className="text-sm text-left text-white">{discription}</p>
         <div className="flex justify-center w-full mt-2">
-          <a
+          <motion.a
             target="_blank"
             className="text-base py-2 px-5 bg-primary rounded-md text-white hover:text-white"
             href={buttonLink}
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.9 }}
           >
             Learn More
-          </a>
+          </motion.a>
         </div>
-      </div>
+      </motion.div>
     </>
   );
 }
