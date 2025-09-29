6f66c09e719fc8d8bb01f09d395cdee74435ef53
Biraj Buddhacharya <birajbuddhacharya@gmail.com>
2025-09-29 11:50:22 +0545

    Bug fix: incorrect layout from previous refactoring solved

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Toaster } from "@/components/ui/sonner";
-import "../globals.css";
+import "./globals.css";
 import Head from 'next/head';
 
 const geistSans = Geist({
